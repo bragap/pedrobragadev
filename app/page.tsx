d6f4e7523118fe537f9ed0a1105fab8a4754fea0
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2024-08-06 19:02:46 -0300

    content

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
                         <p className="text-base sm:text-lg font-medium text-zinc-400 mb-8"> I'm a software engineer and frontend developer focused in UI/UX and interaction design.
                             I specialize in Next.js, React.js and web development.
                         </p>
-                        <div className="flex gap-4">
-                            <div className=" flex items-center bg-zinc-900 py-1 px-3 rounded-3xl hover:bg-zinc-800 transition duration-300">
+                        <div className="flex flex-col space-y-3 sm:flex sm:flex-row gap-4">
+                            <div className=" flex  items-center bg-zinc-900 py-1 px-3 rounded-3xl hover:bg-zinc-800 transition duration-300">
                                 <Link href="https://www.linkedin.com/in/pedrobragadecastro/" target="_blank" className="flex items-center text-zinc-300 gap-1 ">Linkedln <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" className="h-4 w-4 text-zinc-500 "><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z" clip-rule="evenodd"></path></svg></Link>
                             </div>
                             <div className="flex items-center bg-zinc-900 py-1 px-3 rounded-3xl hover:bg-zinc-800 transition duration-300">
@@ -24,7 +24,7 @@ export default function Home() {
                             <div className="flex items-center bg-zinc-900 py-1 px-3 rounded-3xl hover:bg-zinc-800 transition duration-300">
                                 <Link href="https://github.com/bragap" target="_blank" className="flex items-center text-zinc-300 gap-1">GitHub <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" className="h-4 w-4 text-zinc-500"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z" clip-rule="evenodd"></path></svg></Link>
                             </div>
-                            
+
                         </div>
 
                         <span></span>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { MoveUpRight, Github } from "lucide-react";
 import { fetchProjects } from "../lib/data";
 
 
